1489d3d791aea8e5c06953db3a9c7e4d4f39f38c
RaquelFT <102087566+RaquelFT@users.noreply.github.com>
2022-08-30 18:16:44 -0300

    Delete sistemaNotas2.js

--- sistemaNotas2.js
+++
@@ -1,5 +0,0 @@
-function situacao(){
-    var nota1 = prompt("Digite a primeira nota: ");
-    var nota2 = prompt("Digite a segunda nota: ");
-    console.log(21 - (Number(nota1) + Number(nota2)));
-}
